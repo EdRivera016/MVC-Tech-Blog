e4b466b646fdc82a1f3fd28194f6795a208121f0
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-06-02 19:09:47 -0400

    added code to define model attributes

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    // Model attributes are defined here
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
