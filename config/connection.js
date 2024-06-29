45b5845d9c557532f9d366f9c9abe9b85138cf44
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-06-28 23:39:38 -0400

    testing app

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
+// Log environment variables
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
+// Create Sequelize instance
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -11,5 +19,17 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
   }
 );
+
+// Authenticate Sequelize instance
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+
+    // Further code that depends on a successful database connection can go here
+
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
