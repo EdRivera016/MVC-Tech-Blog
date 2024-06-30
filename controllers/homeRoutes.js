7fe6a3d58a217c8a6b2516e4f8e2fdfb1950eba3
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-06-30 19:23:46 -0400

    added bootstrap cdn

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,27 +1,27 @@
+// controllers/homeRoutes.js
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const { Post, User } = require('../models');
 
-// Route for fetching and rendering the homepage
 router.get('/', async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-      ],
-    });
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
 
-    const posts = postData.map((post) => post.get({ plain: true }));
+        const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', {
-      posts,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+        res.render('home', { 
+            posts, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
