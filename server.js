4250282925e20cfbb4bc439fe7e596554bef6996
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-07-01 23:07:08 -0400

    testing

--- server.js
+++ server.js
@@ -1,3 +1,51 @@
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// // Set up Handlebars.js engine with custom helpers
+// const hbs = exphbs.create({ helpers });
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+
+// // Inform Express.js on which template engine to use
+// app.engine('handlebars', hbs.engine({
+//   defaultLayout: 'main',
+//   layoutsDir: path.join(__dirname, 'views/layouts'),
+//   partialsDir: path.join(__dirname, 'views/partials')
+// }));
+
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+// });
+
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -27,13 +75,9 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine({
-  defaultLayout: 'main',
-  layoutsDir: path.join(__dirname, 'views/layouts'),
-  partialsDir: path.join(__dirname, 'views/partials')
-}));
-
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
